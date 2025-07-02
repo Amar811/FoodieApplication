6d44e0a3a948ad6f859e2796fe5dd4516b9901f6
Amar Ram <ramamar151@gmail.com>
2025-07-02 17:34:43 +0530

    foods

---
+++ src/data/foods.jsx
@@ -0,0 +1,25 @@
+const foods = [
+  {
+    id: 1,
+    name: "Margherita Pizza",
+    description: "Classic delight with 100% real mozzarella cheese",
+    price: 199,
+    image: "https://source.unsplash.com/200x200/?pizza"
+  },
+  {
+    id: 2,
+    name: "Veg Burger",
+    description: "Delicious veggie burger with crispy patty",
+    price: 99,
+    image: "https://source.unsplash.com/200x200/?burger"
+  },
+  {
+    id: 3,
+    name: "Pasta",
+    description: "Cheesy white sauce pasta with vegetables",
+    price: 149,
+    image: "https://source.unsplash.com/200x200/?pasta"
+  }
+];
+
+export default foods;
