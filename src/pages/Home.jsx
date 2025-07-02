5d65aeff9dde29acda3a39e865b252cba68f082a
Amar Ram <ramamar151@gmail.com>
2025-07-02 17:34:28 +0530

    Home

---
+++ src/pages/Home.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import foods from '../data/foods'
+
+const Home = () => {
+  return (
+    <>
+    <div className='p-4 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4'>
+       {foods.map((item)=>(
+           <div key={item.id} className='border p-4 rounded shadow'>
+            <img src={item.image} className='w-full h-40 object-cover rounded'/>
+            <h2 className='text-lg font-bold mt-2'>{item.name}</h2>
+            <p className='text-sm'>{item.description}</p>
+            <p className='text-green-600 font-semibold mt-1'>₹{item.price}</p>
+            <button className='mt-2 bg-blue-500 text-white px-3 py-1 rounded'>Add to Cart</button>
+        </div>
+        ))}
+    </div>
+    </>
+  )
+}
+
+export default Home
